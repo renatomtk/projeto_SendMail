49e787063c354fd1bec3a32255e3fe0e5a300170
Renato Motikawa <renatomtk@MacBook-Air-de-Renato.local>
2023-07-11 13:11:56 -0300

    first commit

---
+++ processa_envio.php
@@ -0,0 +1,10 @@
+<?php 
+
+require "../../../require_app_send_mail/libs/PHPMailer/Exception.php";
+require "../../../require_app_send_mail/libs/PHPMailer/OAuthTokenProvider.php";
+require "../../../require_app_send_mail/libs/PHPMailer/OAuth.php";
+require "../../../require_app_send_mail/libs/PHPMailer/PHPMailer.php";
+require "../../../require_app_send_mail/libs/PHPMailer/POP3.php";
+require "../../../require_app_send_mail/libs/PHPMailer/SMTP.php";
+
+require "../../../require_app_send_mail/processa_envio.php";
